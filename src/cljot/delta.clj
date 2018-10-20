b22f210131cef6f73d1a569db1e0ee1c4b4365ab
Petri Myllys <petri.myllys@reaktor.com>
2018-10-20 20:00:04 +0300

    Add ops API for delta

--- src/cljot/delta.clj
+++ src/cljot/delta.clj
@@ -15,3 +15,19 @@
    (if (vector? ops) (apply delta ops) [ops]))
   ([first-op & rest]
    (reduce add-op [first-op] rest)))
+
+(defn insert
+  ([delta text]
+   (add-op delta (make-insert text)))
+  ([delta text attributes]
+   (add-op delta (make-insert text attributes))))
+
+(defn retain
+  ([delta length]
+   (add-op delta (make-retain length)))
+  ([delta length attributes]
+   (add-op delta (make-retain length attributes))))
+
+(defn delete
+  [delta length]
+  (add-op delta (make-delete length)))
