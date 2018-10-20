40315ffcf294a98bd89e9ad24ebb77227541cfc8
Petri Myllys <pietrorossellini@gmail.com>
2018-10-20 11:48:48 +0300

    Lay out the project foundation

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to Cljot
+
+Cljot is an operational transformation format with support for concurrent rich text editing.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljot "0.1.0-SNAPSHOT"
+  :description "Operational transformation format with support for concurrent rich text editing"
+  :url "https://github.com/Pietrorossellini/cljot"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
