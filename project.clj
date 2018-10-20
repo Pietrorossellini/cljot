cce2938485dff8fa8ee4d18c78d06cb0e8c60302
Petri Myllys <pietrorossellini@gmail.com>
2018-10-20 13:18:40 +0300

    Add test coverage reporting

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/Pietrorossellini/cljot"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cloverage "1.0.13"]])
